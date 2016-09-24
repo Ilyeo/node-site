4cf3d0ec2bb12b85a89a54df5c5ac5f311b7330d
Rogelio Alatorre <rojo.perfeccion@gmail.com>
2016-09-24 14:21:44 -0500

    Agregando la dependencia del archivo routes.js en server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
+var router = require('./app/routes');
+
 app.listen(port, function() {
   console.log('app started');
 });
