8885712cba85f70479f3b583757603e48252e50e
Rogelio Alatorre <rojo.perfeccion@gmail.com>
2016-09-24 14:25:28 -0500

    Agregando las rutas a nuestra variable app

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
+// route our app
 var router = require('./app/routes');
+app.use('/',router);
 
 app.listen(port, function() {
   console.log('app started');
