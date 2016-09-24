d0ac31ad26d558a8a8a4744ad5e3e66000db4c23
Rogelio Alatorre <rojo.perfeccion@gmail.com>
2016-09-24 14:18:29 -0500

    Moviendo codigo de rutas del archivo /server.js al /app/routes.js

---
+++ app/routes.js
@@ -0,0 +1,12 @@
+// route for our homepage
+app.get('/', function(req, res) {
+  res.send('hello roger!');
+});
+
+// route for our about page
+app.get('/about', function(req, res) {
+  res.send('hello about!');
+});
+
+app.get('/contact');
+app.post('/contact');
--- server.js
+++ server.js
@@ -5,16 +5,3 @@ var port = 8080;
 app.listen(port, function() {
   console.log('app started');
 });
-
-// route for our homepage
-app.get('/', function(req, res) {
-  res.send('hello roger!');
-});
-
-// route for our about page
-app.get('/about', function(req, res) {
-  res.send('hello about!');
-});
-
-app.get('/contact');
-app.post('/contact');
