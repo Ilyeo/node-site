c5a72314ecde2e23b0c46e76966eaf11a077b713
Rogelio Alatorre <rojo.perfeccion@gmail.com>
2016-09-24 13:31:11 -0500

    Limpiando el archivo server.js ( posteriormente se implementara uno con express )

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-var http = require('http');
 
-function handleRequests(req,res) {
-  res.end('Hello roger!');
-}
-
-var server = http.createServer(handleRequests);
-
-server.listen(8080, function() {
-  console.log('Listening on port 8080');
-});
